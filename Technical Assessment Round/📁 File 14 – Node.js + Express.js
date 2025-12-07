12bd709005cb23eaa8afe13915106658163ae923
AREVETI SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-12-07 08:01:54 +0530

    Create 📁 File 14 – Node.js + Express.js

---
+++ Technical Assessment Round/📁 File 14 – Node.js + Express.js
@@ -0,0 +1,21 @@
+📁 File 14 – Node.js + Express.js
+
+What is Node.js?
+
+What is NPM?
+
+What is Express.js?
+
+What is middleware?
+
+What is REST API?
+
+What is JSON?
+
+Difference between PUT and PATCH.
+
+What is routing?
+
+What is CORS?
+
+What is async programming in Node.js?
