c66d8bec8c201d26f9685c77264f90edc5039d71
AREVETI SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-12-07 08:01:37 +0530

    Create 📁 File 13 – React.js

---
+++ Technical Assessment Round/📁 File 13 – React.js
@@ -0,0 +1,21 @@
+📁 File 13 – React.js
+
+What is React?
+
+What is JSX?
+
+What is virtual DOM?
+
+What are components?
+
+What are props?
+
+What is state?
+
+What are React hooks?
+
+What is useState?
+
+What is useEffect?
+
+What is context API?
